64b9294410978bce3dc94e243a6284d437207b4f
KariVI <47987287+KariVI@users.noreply.github.com>
2022-11-27 19:37:53 -0600

    Se añade la división de paquetes

---
+++ backend/ProInverstServer/src/java/basic/AccessWS.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basic;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author kari
+ */
+@Path("api")
+public class AccessWS {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of AccessWS
+     */
+    public AccessWS() {
+    }
+
+    /**
+     * Retrieves representation of an instance of basic.AccessWS
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public String getXml() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of AccessWS
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public void putXml(String content) {
+    }
+}
