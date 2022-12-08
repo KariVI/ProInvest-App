cf0fa1d48bf5f4337efef5f5abfcc02d4014c246
WastyFace <wastyface93@gmail.com>
2022-12-08 15:04:44 -0600

    Actualización Clases

--- backend/Segunda version/ProInverstServer/src/java/basic/AccessWS.java
+++ backend/Segunda version/ProInverstServer/src/java/basic/AccessWS.java
@@ -16,6 +16,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
+import pojos.Mensaje;
 import pojos.Respuesta;
 import pojos.SesionToken;
 import pojos.User;
@@ -29,46 +30,67 @@ import seguridad.AutorizacionTokenJWT;
 @Path("access")
 public class AccessWS {
     
+//    @POST
+//    @Path("login")
+//    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Respuesta login(
+//            @FormParam("email") String email,
+//            @FormParam("contrasena") String contrasena
+//    ) {
+//        Respuesta res = new Respuesta();
+//        //--------VALIDAR PARAMETROS DE ENTRADA--------------//
+//        if(email==null || email.trim().isEmpty()){
+//            res.setError(true);
+//            res.setMensaje("El email es un dato requerido...");
+//            return res;
+//        }else if(contrasena==null || contrasena.trim().isEmpty()){
+//            res.setError(true);
+//            res.setMensaje("La contraseña es un dato requerido...");
+//            return res;
+//        }
+//        //--------VALIDAR CREDENCIALES DEL EMPLEADO----------//
+//        User u = UserDAO.login(email, contrasena);
+//        if(u==null) {
+//            res.setError(true);
+//            res.setMensaje("No se encontró ningún empleado con esas credenciales...");
+//            return res;
+//        }
+//        //------GENERAR TOKEN CON JWT Y DEVOLVERLO-----------//
+//        SesionToken s = new SesionToken(); s.setId(u.getIdUser());
+//        s.setEmail(u.getCorreo());
+//        s = AutorizacionTokenJWT.generarToken(s);
+//        if(s==null || s.getTokenacceso()==null || s.getTokenacceso().isEmpty()){
+//            res.setError(true);
+//            res.setMensaje("No se puede generar el token de acceso...");
+//        }else{
+//            res.setError(false);
+//            res.setMensaje("Bienvenido: "+s.getEmail());
+//            res.setSesiontoken(s);
+//        }
+//        //---------------------------------------------------//
+//        return res;
+//    }
+    
     @POST
     @Path("login")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
-    public Respuesta login(
-            @FormParam("email") String email,
+    public Mensaje loginUsuario(
+            @FormParam("correo") String correo,
             @FormParam("contrasena") String contrasena
-    ) {
-        Respuesta res = new Respuesta();
-        //--------VALIDAR PARAMETROS DE ENTRADA--------------//
-        if(email==null || email.trim().isEmpty()){
-            res.setError(true);
-            res.setMensaje("El email es un dato requerido...");
-            return res;
-        }else if(contrasena==null || contrasena.trim().isEmpty()){
-            res.setError(true);
-            res.setMensaje("La contraseña es un dato requerido...");
-            return res;
+    ){
+        Mensaje resultado;
+        
+        User u = new User();
+        
+        boolean res = UserDAO.loginUser(correo, contrasena);
+        if (res) {
+            resultado = new Mensaje("Usuario encontrado, you ate", false);
+        } else {
+            resultado = new Mensaje("Error, you thought you ate but u didn't", true);
         }
-        //--------VALIDAR CREDENCIALES DEL EMPLEADO----------//
-        User u = UserDAO.login(email, contrasena);
-        if(u==null) {
-            res.setError(true);
-            res.setMensaje("No se encontró ningún empleado con esas credenciales...");
-            return res;
-        }
-        //------GENERAR TOKEN CON JWT Y DEVOLVERLO-----------//
-        SesionToken s = new SesionToken(); s.setId(u.getIdUser());
-        s.setEmail(u.getCorreo());
-        s = AutorizacionTokenJWT.generarToken(s);
-        if(s==null || s.getTokenacceso()==null || s.getTokenacceso().isEmpty()){
-            res.setError(true);
-            res.setMensaje("No se puede generar el token de acceso...");
-        }else{
-            res.setError(false);
-            res.setMensaje("Bienvenido: "+s.getEmail());
-            res.setSesiontoken(s);
-        }
-        //---------------------------------------------------//
-        return res;
+        return resultado;
     }
     
     @POST
