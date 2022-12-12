ed6fdc907ddc51ef9f95937a37401f105154f56c
DavidMijangos08 <78769160+DavidMijangos08@users.noreply.github.com>
2022-12-12 17:53:57 -0600

    Actualizaciòn de clases

--- backend/Segunda version/ProInverstServer/src/java/basic/DatoSepomexWS.java
+++ backend/Segunda version/ProInverstServer/src/java/basic/DatoSepomexWS.java
@@ -5,6 +5,8 @@
  */
 package basic;
 
+import dao.DatoSepomexDAO;
+import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
@@ -12,7 +14,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import pojos.DatoSepomex;
 
 /**
  * REST Web Service
@@ -26,5 +30,16 @@ public class DatoSepomexWS {
     public DatoSepomexWS() {
     }
 
-   
+    @GET
+    @Path("getByCp/{cp}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<DatoSepomex> getByCp(
+                @PathParam("cp") Integer cp
+    ){
+        List<DatoSepomex> lista = DatoSepomexDAO.getByCp(cp);    
+        return lista;
+    }
+    
+    
+    
 }
