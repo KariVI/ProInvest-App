e07429d590d92b0b3acdd7441c095643c7425290
WastyFace <wastyface93@gmail.com>
2022-12-14 19:02:25 -0600

    Update AccessWS.java

--- backend/ProInvestServer/src/java/basic/AccessWS.java
+++ backend/ProInvestServer/src/java/basic/AccessWS.java
@@ -64,6 +64,6 @@ public class AccessWS {
         } else {
             resultado = new Mensaje("Error, you thought you ate but u didn't", true);
         }
-        return Response.status(Response.Status.OK).header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD").header("Access-Control-Allow-Origin", "*").entity(new Gson().toJson(resultado)).build();
+        return Response.status(Response.Status.OK).header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD").header("Access-Control-Allow-Origin", "*").header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With").header("Access-Control-Allow-Credentials", "true").entity(new Gson().toJson(resultado)).build();
     }    
 }
