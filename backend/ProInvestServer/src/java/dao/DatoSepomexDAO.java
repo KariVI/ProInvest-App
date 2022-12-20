6399ac820e335548fdf07eeae9cee174d130947c
DavidMijangos08 <78769160+DavidMijangos08@users.noreply.github.com>
2022-12-19 21:28:37 -0600

    recuperaciòn cambios

--- backend/ProInvestServer/src/java/dao/DatoSepomexDAO.java
+++ backend/ProInvestServer/src/java/dao/DatoSepomexDAO.java
@@ -21,7 +21,7 @@ public class DatoSepomexDAO {
             + "?allowPublicKeyRetrieval=true&useSSL=false";
 
     private static String username = "root";
-    private static String password = "Fairy59Atomic12";
+    private static String password = "amoapumas";
 
     public static Connection abrirConexionBD() {
         Connection c = null;
--- backend/ProInvestServer/src/java/dao/OrigenFondoDAO.java
+++ backend/ProInvestServer/src/java/dao/OrigenFondoDAO.java
@@ -20,7 +20,7 @@ public class OrigenFondoDAO {
             + "?allowPublicKeyRetrieval=true&useSSL=false";
 
     private static String username = "root";
-    private static String password = "Fairy59Atomic12";
+    private static String password = "";
 
     public static Connection abrirConexionBD() {
         Connection c = null;
